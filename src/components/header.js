00322da77cc7b96b1421717b681c2927dddf8273
EverittGill <eg.everittgill@gmail.com>
2023-06-12 13:28:09 -0400

    got the header component connected and rendering

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// this is the header component. It will render the header when used
+function Header () {
+    return( <>
+    <div>
+        <p> The header component is connected and is rendering</p>
+    </div>
+    </>
+    )
+}
+
+export default Header
