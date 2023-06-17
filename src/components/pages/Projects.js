098e8f4b58e69c32d4c07073884f77466dd05e20
EverittGill <eg.everittgill@gmail.com>
2023-06-17 18:44:00 -0400

    got 6 cards to display properly

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,25 +1,50 @@
 function Projects() {
     return (
+        <>
         <div className="wrapper">
           <Card
-            img="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            img="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/The-vault-screenshot.png?raw=true"
             title="The Everyday Salad"
             description="Take your boring salads up a knotch. This recipe is perfect for lunch
               and only contains 5 ingredients!"
           />
     
           <Card
-            img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            img="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/first-page-screenshot.png?raw=true"
             title="Simple Risotto"
             description="Fear Risotto no more! This simple recipe is perfect for family dinners."
           />
     
           <Card
-            img="https://images.unsplash.com/photo-1529928520614-7c76e2d99740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            img="https://github.com/EverittGill/Everitt-Gill-React-Portfolio/blob/main/src/assets/images/E-commerce-pic.jpeg?raw=true"
             title="Baked Cod with Vegetables"
             description="Baked Cod with Vegetables. 30 minute meal!"
           />
+
         </div>
+        <br></br>
+        <div className="wrapper">
+          <Card
+            img="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/The-vault-screenshot.png?raw=true"
+            title="The Everyday Salad"
+            description="Take your boring salads up a knotch. This recipe is perfect for lunch
+              and only contains 5 ingredients!"
+          />
+    
+          <Card
+            img="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/first-page-screenshot.png?raw=true"
+            title="Simple Risotto"
+            description="Fear Risotto no more! This simple recipe is perfect for family dinners."
+          />
+    
+          <Card
+            img="https://github.com/EverittGill/Everitt-Gill-React-Portfolio/blob/main/src/assets/images/E-commerce-pic.jpeg?raw=true"
+            title="Baked Cod with Vegetables"
+            description="Baked Cod with Vegetables. 30 minute meal!"
+          />
+
+        </div>
+        </>
       );
     }
     
