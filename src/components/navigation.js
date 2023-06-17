b1170a1c4b20a787036f2d5389508214d692e409
EverittGill <eg.everittgill@gmail.com>
2023-06-17 17:59:48 -0400

    added a new projects section and some pictures

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,41 +6,49 @@
 // import Contact from "./pages/Contact"
 // import Portfolio from "./Portfolio";
 
+import Nav from 'react-bootstrap/Nav';
+
+
+// function Navigation ({currentPage, handlePageChange}) {
+
+
+//     return (
+//         <>
+//         <nav>
+//         <div class="container-top">
+//          <ul class="nav-items">
+//             <li><a href="#about-me" onClick={ () => handlePageChange ("about me")} >About Me</a></li>
+//             <li><a href="#my-work" onClick={ () => handlePageChange ("projects")}>projects</a></li>
+//             <li><a href="#resume" onClick={ () => handlePageChange ("contact")} >Contact</a></li>
+//             <li><a href="#contact" onClick={ () => handlePageChange ("resume")} >Resume</a></li>
+//          </ul>
+//         </div>
+//     </nav>
+//         </>
+//     )
+// }
+
+// export default Navigation;
 
 
 
-function Navigation ({currentPage, handlePageChange}) {
-
-    // const renderPage = () => {
-    //     switch (currentPage) {
-    //       case 'about me':
-    //         return <About />;
-    //       case 'porfolio':
-    //         return <Portfolio />;
-    //       case 'contact':
-    //         return <Contact />;
-    //       case 'resume':
-    //         return <Resume />;
-    //       default:
-    //         return <About />;
-    //     }
-    //   };
-
-// remove image from nav bar and put it in the header
-    return (
-        <>
-        <nav>
-        <div class="container-top">
-         <ul class="nav-items">
-            <li><a href="#about-me" onClick={ () => handlePageChange ("about me")} >About Me</a></li>
-            <li><a href="#my-work" onClick={ () => handlePageChange ("projects")}>projects</a></li>
-            <li><a href="#resume" onClick={ () => handlePageChange ("contact")} >Contact</a></li>
-            <li><a href="#contact" onClick={ () => handlePageChange ("resume")} >Resume</a></li>
-         </ul>
-        </div>
-    </nav>
-        </>
-    )
+function Navigation({currentPage, handlePageChange}) {
+  return (
+    <Nav variant="pills" defaultActiveKey="#about-me">
+      <Nav.Item>
+        <Nav.Link href="#about-me" onClick={ () => handlePageChange ("about me")}>About Me</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="#projects" onClick={ () => handlePageChange ("projects")}>Projects</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="resume" onClick={ () => handlePageChange ("resume")} >Resume</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="contact" onClick={ () => handlePageChange ("contact")} >Contact Me</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
 }
 
 export default Navigation;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,65 +1,143 @@
-function Mywork() {
-    return (
-        <>
-        <section class="site-section" id="my-work">
-        <h2 class="h2-header">My Work</h2>
-         <div>
-             <div class="projects">
-                 <a href="https://everittgill.github.io/Horiseon-Home-Page/">
-                    <img src="images/Horiseon-screenshot.png" id="projects-image" alt="project-1"></img></a>
-                 <div class="mini-card-first">
-                     <p>Horiseon Digital Marketing</p>
-                 </div>
-                </div>
-         <div class="project-case">
-             <div>
-                
-                 <a href="https://everittgill.github.io/Soccer-Betting-Mini-Game/">
-                 <img src="images/first-page-screenshot.png" id="projects-image" alt="project-2"></img></a>
-                 <div class="mini-card">
-                     <p>Soccer Betting Game</p>
-                 </div>
-             </div>
-             <div>
-                
-                    <a href="https://everittgill.github.io/Interactive-Budapest-Map/">
-                 <img src="images/BudapestMap.jpg" id="projects-image" alt="project-3"></img></a>
-                 <div class="mini-card">
-                     <p>Interactive Budapest Map</p>
-                 </div>
-             </div>
-             <div>
-                
-                 <a href="https://thegoldenvault.herokuapp.com">
-                 <img src="images/The-vault-screenshot.png" id="projects-image" alt="project-4"></img></a>
-                 <div class="mini-card">
-                     <p>The Vault</p>
-                     <p>Password Saver</p>
-                 </div>
-             </div>
-             <div>
-                
-                    <a href="https://twitter-clone-eg.herokuapp.com/">
-                 <img src="images/CADClone.jpg" id="projects-image" alt="project-5"></img></a>
-                 <div class="mini-card">
-                     <p>Twitter clone</p>
-                 </div>
-             </div>
-             <div>
-                
-                    <a href="https://twitter-clone-eg.herokuapp.com/">
-                <img src="images/CADClone.jpg" id="projects-image" alt="Project-6"></img></a>
-                <div class="mini-card">
-                    <p>code generator</p>
-                </div>
-         </div>
-         </div>
-             
-            </div>
-     </section>
-            </>
-    
-    )
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function GroupExample() {
+  return (
+    <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/The-vault-screenshot.png?raw=true" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/first-page-screenshot.png?raw=true" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+  );
 }
 
-export default Mywork;
+export default GroupExample;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Mywork() {
+//     return (
+//         <>
+//         <section class="site-section" id="my-work">
+//         <h2 class="h2-header">My Work</h2>
+//          <div>
+//              <div class="projects">
+//                  <a href="https://everittgill.github.io/Horiseon-Home-Page/">
+//                     <img src="images/Horiseon-screenshot.png" id="projects-image" alt="project-1"></img></a>
+//                  <div class="mini-card-first">
+//                      <p>Horiseon Digital Marketing</p>
+//                  </div>
+//                 </div>
+//          <div class="project-case">
+//              <div>
+                
+//                  <a href="https://everittgill.github.io/Soccer-Betting-Mini-Game/">
+//                  <img src="images/first-page-screenshot.png" id="projects-image" alt="project-2"></img></a>
+//                  <div class="mini-card">
+//                      <p>Soccer Betting Game</p>
+//                  </div>
+//              </div>
+//              <div>
+                
+//                     <a href="https://everittgill.github.io/Interactive-Budapest-Map/">
+//                  <img src="images/BudapestMap.jpg" id="projects-image" alt="project-3"></img></a>
+//                  <div class="mini-card">
+//                      <p>Interactive Budapest Map</p>
+//                  </div>
+//              </div>
+//              <div>
+                
+//                  <a href="https://thegoldenvault.herokuapp.com">
+//                  <img src="images/The-vault-screenshot.png" id="projects-image" alt="project-4"></img></a>
+//                  <div class="mini-card">
+//                      <p>The Vault</p>
+//                      <p>Password Saver</p>
+//                  </div>
+//              </div>
+//              <div>
+                
+//                     <a href="https://twitter-clone-eg.herokuapp.com/">
+//                  <img src="images/CADClone.jpg" id="projects-image" alt="project-5"></img></a>
+//                  <div class="mini-card">
+//                      <p>Twitter clone</p>
+//                  </div>
+//              </div>
+//              <div>
+                
+//                     <a href="https://twitter-clone-eg.herokuapp.com/">
+//                 <img src="images/CADClone.jpg" id="projects-image" alt="Project-6"></img></a>
+//                 <div class="mini-card">
+//                     <p>code generator</p>
+//                 </div>
+//          </div>
+//          </div>
+             
+//             </div>
+//      </section>
+//             </>
+    
+//     )
+// }
+
+// export default Mywork;
+
+
+
+
