d3b6fb2e10f634013e7974f9d669df11d9214ba4
EverittGill <eg.everittgill@gmail.com>
2023-06-19 21:14:37 -0400

    minor update

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,8 @@ function Resume() {
     <section class="site-section" id="resume">
         <h2 class="h2-header">My Resume</h2>
         <Container className="d-flex justify-content-center">
-          <a href="/assets/Everitt-Gill-Resume.pdf" download="Everitt-Gill-Resume.pdf">
+          {/* add an href in an anchor tag that has the file Everitt-Gill-Resume */}
+          <a href="../assets/Everitt-Gill-Resume" download="Everitt-Gill-Resume.pdf">
             <img className='image-fluid resume-image' src="https://github.com/EverittGill/Everitt-Gill-Webiste/blob/main/images/EverittGillResume.jpg?raw=true" id="resume-image" alt="Everitt Gill resume" />
           </a>
           </Container>
