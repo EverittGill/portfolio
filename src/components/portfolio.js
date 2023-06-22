ed752e40d722c715f6f0742620bbdbacd08a7ec4
EverittGill <eg.everittgill@gmail.com>
2023-06-21 21:40:22 -0400

    push to make sure everything is pushed

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,22 +7,11 @@ import Projects from './pages/Projects'
 
 
 
+
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
   
-    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-    // const renderPage = () => {
-    //   if (currentPage === 'About') {
-    //     return <About />;
-    //   }
-    //   if (currentPage === 'Contact') {
-    //     return <Contact />;
-    //   }
-    //   if (currentPage === 'Blog') {
-    //     return <Projects />;
-    //   }
-    //   return <About />;
-    // };
+    
 
         const renderPage = () => {
         switch (currentPage) {
