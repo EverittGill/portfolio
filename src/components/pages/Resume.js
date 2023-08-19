f6d9052c747fb5b799104a266106ee781108a3b3
EverittGill <eg.everittgill@gmail.com>
2023-08-19 15:56:01 -0400

    added the new resume and gave the ability to download it

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { Container } from 'react-bootstrap';
-import ResumeImg from '../../assets/images/Everitt-Gill-Resume.jpg'
+import Pdf from '../../assets/Everitt_Gill_Resume_B&W_2023.docx.pdf';  
+import ResumeImg from '../../assets/images/Everitt_Gill_Resume_B&W_2023.docx.jpg';
 
 function Resume() {
   return (
     <>
-    <div>
-    <section class="site-section" id="resume">
-        <h2 class="h2-header">My Resume</h2>
-        <Container className="d-flex justify-center max-w-full">
-          {/* add an href in an anchor tag that has the file Everitt-Gill-Resume */}
-          <a href="../assets/Everitt-Gill-Resume" download="Everitt-Gill-Resume.pdf" style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
-            <img className='image-fluid resume-image' src= { ResumeImg } id="resume-image" alt="Everitt Gill resume" />
-          </a>
+      <div>
+        <section className="site-section" id="resume">
+          <h2 className="h2-header">My Resume</h2>
+          <Container className="d-flex justify-center max-w-full">
+            <a 
+              href={Pdf} 
+              download="Everitt_Gill_Resume_B&W_2023.docx.pdf" 
+              style={{display: 'flex', justifyContent: 'center', width: '100%'}}
+            >
+              <img 
+                className='image-fluid resume-image border-solid border-4 border-black rounded-lg' 
+                src={ResumeImg} 
+                id="resume-image" 
+                alt="Everitt Gill resume" 
+              />
+            </a>
           </Container>
-          </section>
-    </div>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
+
+
